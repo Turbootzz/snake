09f07fb86c8ab0667b630e323f8df9b0893483b7
Thijs <tc.herman@hotmail.com>
2021-03-16 09:34:46 +0100

    Food Added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-let boardSize = 11;
+let boardSize = 15;
 let board = document.getElementById("board");
 let keyboardInput = document.getElementById("keyboardInput");
 let direction = 0
-let position = { x: Math.floor((boardSize - 1) / 2), y: Math.floor((boardSize - 1) / 2) };
+let foodIsEaten = true;
+let snakePosition = { x: Math.floor((boardSize - 1) / 2), y: Math.floor((boardSize - 1) / 2) };
+let foodPosition = {x:0,y:0};
 
 //het board
 function drawBoard() {
@@ -16,30 +18,39 @@ function drawBoard() {
 
 //clearboard
 
+
 function clearBoard() {
+    /*
     for (let j = 0; j < boardSize; j++) {
         for (let i = 0; i < boardSize; i++) {
             let snakeId = "x" + i + "y" + j;
             document.getElementById(snakeId).style.background = "gray";
         }
     }
+*/
+
+    //kan sneller met hieronder
+
+    document.querySelectorAll('.cell').forEach(function (cell) {
+        cell.className = "cell";
+    });
 }
 
 
 //update de positie
 
-function updatePosition() {
+function updatesnakePosition() {
     if (direction == "up") {
-        position.y = position.y - 1;
+        snakePosition.y = snakePosition.y - 1;
     }
     if (direction == "down") {
-        position.y = position.y + 1;
+        snakePosition.y = snakePosition.y + 1;
     }
     if (direction == "right") {
-        position.x = position.x + 1;
+        snakePosition.x = snakePosition.x + 1;
     }
     if (direction == "left") {
-        position.x = position.x - 1;
+        snakePosition.x = snakePosition.x - 1;
     }
 }
 
@@ -47,32 +58,49 @@ function updatePosition() {
 
 function resetGame() {
     direction = 0;
-    position = { x: Math.floor((boardSize - 1) / 2), y: Math.floor((boardSize - 1) / 2) };
+    snakePosition = { x: Math.floor((boardSize - 1) / 2), y: Math.floor((boardSize - 1) / 2) };
 }
 
 //gameOver
 
 function collisionCheck() {
-    if (position.x < 0 || position.y < 0 || position.x > boardSize - 1 || position.y > boardSize - 1) { resetGame() }
+    if (snakePosition.x < 0 || snakePosition.y < 0 || snakePosition.x > boardSize - 1 || snakePosition.y > boardSize - 1) { resetGame() }
 }
 
 
 function drawSnake() {
-    let snakeHeadPosition = "x" + position.x + "y" + position.y;
-    document.getElementById(snakeHeadPosition).style.background = "red";
+    let snakeHeadsnakePosition = "x" + snakePosition.x + "y" + snakePosition.y;
+    document.getElementById(snakeHeadsnakePosition).className += " bodySnake";
 }
 
-drawBoard(); //tekent het board
+
+ //teken het food
+function drawFood() {
+    if (foodIsEaten) {
+        let xRandom = Math.floor(Math.random() * (boardSize-1));
+        let yRandom = Math.floor(Math.random() * (boardSize-1));
+        foodPosition.x = xRandom;
+        foodPosition.y = yRandom;
+        foodIsEaten = false;
+    }
+    let foodPositionID = "x" + foodPosition.x + "y" + foodPosition.y;
+    document.getElementById(foodPositionID).className += " food";
+}
+
 
 //Game
 function gameLoop()
 {
-    updatePosition();
+    updatesnakePosition();
     collisionCheck();
     clearBoard();
+    drawFood();
     drawSnake();
 }
 
+//game start
+
+drawBoard(); //tekent het board
 setInterval(gameLoop, 500);
 
 //keyboard controls
@@ -90,5 +118,6 @@ window.addEventListener("keydown", function (event) {
     if (event.key == "ArrowLeft") {
         direction = "left";
     }
-    keyboardInput.innerHTML = direction;
+
+keyboardInput.innerHTML = "direction: " + direction;
 }, true);
